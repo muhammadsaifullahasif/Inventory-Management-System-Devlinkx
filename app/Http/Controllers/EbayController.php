f534a70fb80149cb7d0a2ec5d12285664043b086
Muhammad Saifullah Asif <muhammadsaifullahasif@gmail.com>
2026-01-15 21:05:44 +0500

    Imporvements and Bug Fixes

--- app/Http/Controllers/EbayController.php
+++ app/Http/Controllers/EbayController.php
@@ -13,28 +13,28 @@ class EbayController extends Controller
     private const EBAY_TOKEN_URL = 'https://api.ebay.com/identity/v1/oauth2/token';
     private const API_COMPATIBILITY_LEVEL = '967';
     private const API_SITE_ID = '0'; // US
-    
+
     public function __construct(protected EbayService $ebayService) {}
 
     /**
      * Get active listings (paginated)
      */
-    public function getActiveListings(Request $request, string $id)
-    {
-        try {
-            $salesChannel = $this->getSalesChannelWithValidToken($id);
+    // public function getActiveListings(Request $request, string $id)
+    // {
+    //     try {
+    //         $salesChannel = $this->getSalesChannelWithValidToken($id);
 
-            $listings = $this->ebayService->getActiveListings(
-                $salesChannel,
-                $request->input('page', 1),
-                $request->input('per_page', 100)
-            );
+    //         $listings = $this->ebayService->getActiveListings(
+    //             $salesChannel,
+    //             $request->input('page', 1),
+    //             $request->input('per_page', 100)
+    //         );
 
-            return response()->json($listings);
-        } catch (\Exception $e) {
-            return $this->errorResponse($e->getMessage());
-        }
-    }
+    //         return response()->json($listings);
+    //     } catch (\Exception $e) {
+    //         return $this->errorResponse($e->getMessage());
+    //     }
+    // }
 
     /**
      * Get ALL active listings
@@ -424,134 +424,134 @@ class EbayController extends Controller
     /**
      * Get unsold listings (paginated)
      */
-    public function getUnsoldListings(Request $request, string $id)
-    {
-        try {
-            $salesChannel = $this->getSalesChannelWithValidToken($id);
+    // public function getUnsoldListings(Request $request, string $id)
+    // {
+    //     try {
+    //         $salesChannel = $this->getSalesChannelWithValidToken($id);
 
-            $listings = $this->ebayService->getUnsoldListings(
-                $salesChannel,
-                $request->input('page', 1),
-                $request->input('per_page', 100),
-                $request->input('days', 60)
-            );
+    //         $listings = $this->ebayService->getUnsoldListings(
+    //             $salesChannel,
+    //             $request->input('page', 1),
+    //             $request->input('per_page', 100),
+    //             $request->input('days', 60)
+    //         );
 
-            return response()->json($listings);
-        } catch (\Exception $e) {
-            return $this->errorResponse($e->getMessage());
-        }
-    }
+    //         return response()->json($listings);
+    //     } catch (\Exception $e) {
+    //         return $this->errorResponse($e->getMessage());
+    //     }
+    // }
 
     /**
      * Get ALL unsold listings
      */
-    public function getAllUnsoldListings(Request $request, string $id)
-    {
-        try {
-            $salesChannel = $this->getSalesChannelWithValidToken($id);
-            $listings = $this->ebayService->getAllUnsoldListings(
-                $salesChannel,
-                $request->input('days', 60)
-            );
+    // public function getAllUnsoldListings(Request $request, string $id)
+    // {
+    //     try {
+    //         $salesChannel = $this->getSalesChannelWithValidToken($id);
+    //         $listings = $this->ebayService->getAllUnsoldListings(
+    //             $salesChannel,
+    //             $request->input('days', 60)
+    //         );
 
-            return response()->json($listings);
-        } catch (\Exception $e) {
-            return $this->errorResponse($e->getMessage());
-        }
-    }
+    //         return response()->json($listings);
+    //     } catch (\Exception $e) {
+    //         return $this->errorResponse($e->getMessage());
+    //     }
+    // }
 
     /**
      * Get single item details
      */
-    public function getItemDetails(string $id, string $itemId)
-    {
-        try {
-            $salesChannel = $this->getSalesChannelWithValidToken($id);
-            $itemDetails = $this->ebayService->getItemDetails($salesChannel, $itemId);
+    // public function getItemDetails(string $id, string $itemId)
+    // {
+    //     try {
+    //         $salesChannel = $this->getSalesChannelWithValidToken($id);
+    //         $itemDetails = $this->ebayService->getItemDetails($salesChannel, $itemId);
 
-            return response()->json($itemDetails);
-        } catch (\Exception $e) {
-            return $this->errorResponse($e->getMessage());
-        }
-    }
+    //         return response()->json($itemDetails);
+    //     } catch (\Exception $e) {
+    //         return $this->errorResponse($e->getMessage());
+    //     }
+    // }
 
     /**
      * Redirect user to eBay for authorization
      */
-    public function redirectToEbay(string $id)
-    {
-        $salesChannel = SalesChannel::findOrFail($id);
-        $state = $id . '|' . bin2hex(random_bytes(16));
-        session(['ebay_oauth_state' => $state]);
+    // public function redirectToEbay(string $id)
+    // {
+    //     $salesChannel = SalesChannel::findOrFail($id);
+    //     $state = $id . '|' . bin2hex(random_bytes(16));
+    //     session(['ebay_oauth_state' => $state]);
 
-        return redirect()->away($this->ebayService->getAuthorizationUrl($salesChannel, $state));
-    }
+    //     return redirect()->away($this->ebayService->getAuthorizationUrl($salesChannel, $state));
+    // }
 
     /**
      * Handle OAuth callback from eBay
      */
-    public function callback(Request $request)
-    {
-        \Log::info('eBay OAuth Callback', [
-            'all_params' => $request->all(),
-            'session_state' => session('ebay_oauth_state'),
-        ]);
+    // public function callback(Request $request)
+    // {
+    //     \Log::info('eBay OAuth Callback', [
+    //         'all_params' => $request->all(),
+    //         'session_state' => session('ebay_oauth_state'),
+    //     ]);
 
-        try {
-            $stateParam = $request->input('state');
-            $sessionState = session('ebay_oauth_state');
+    //     try {
+    //         $stateParam = $request->input('state');
+    //         $sessionState = session('ebay_oauth_state');
 
-            if ($stateParam !== $sessionState) {
-                throw new \Exception('Invalid state parameter');
-            }
+    //         if ($stateParam !== $sessionState) {
+    //             throw new \Exception('Invalid state parameter');
+    //         }
 
-            $stateParts = explode('|', $stateParam);
-            $salesChannelId = $stateParts[0] ?? null;
+    //         $stateParts = explode('|', $stateParam);
+    //         $salesChannelId = $stateParts[0] ?? null;
 
-            if (!$salesChannelId) {
-                throw new \Exception('Sales channel ID not found in state');
-            }
+    //         if (!$salesChannelId) {
+    //             throw new \Exception('Sales channel ID not found in state');
+    //         }
 
-            $salesChannel = SalesChannel::findOrFail($salesChannelId);
+    //         $salesChannel = SalesChannel::findOrFail($salesChannelId);
 
-            if ($request->has('error')) {
-                throw new \Exception('eBay authorization failed: ' . $request->input('error_description', $request->input('error')));
-            }
+    //         if ($request->has('error')) {
+    //             throw new \Exception('eBay authorization failed: ' . $request->input('error_description', $request->input('error')));
+    //         }
 
-            $code = $request->input('code');
-            $tokenData = $this->ebayService->getUserAccessToken($salesChannel, $code);
+    //         $code = $request->input('code');
+    //         $tokenData = $this->ebayService->getUserAccessToken($salesChannel, $code);
 
-            $salesChannel->authorization_code = $code;
-            $salesChannel->access_token = $tokenData['access_token'];
-            $salesChannel->access_token_expires_at = now()->addSeconds($tokenData['expires_in']);
+    //         $salesChannel->authorization_code = $code;
+    //         $salesChannel->access_token = $tokenData['access_token'];
+    //         $salesChannel->access_token_expires_at = now()->addSeconds($tokenData['expires_in']);
 
-            if (isset($tokenData['refresh_token'])) {
-                $salesChannel->refresh_token = $tokenData['refresh_token'];
-                if (isset($tokenData['refresh_token_expires_in'])) {
-                    $salesChannel->refresh_token_expires_at = now()->addSeconds($tokenData['refresh_token_expires_in']);
-                }
-            }
+    //         if (isset($tokenData['refresh_token'])) {
+    //             $salesChannel->refresh_token = $tokenData['refresh_token'];
+    //             if (isset($tokenData['refresh_token_expires_in'])) {
+    //                 $salesChannel->refresh_token_expires_at = now()->addSeconds($tokenData['refresh_token_expires_in']);
+    //             }
+    //         }
 
-            $salesChannel->save();
+    //         $salesChannel->save();
 
-            return redirect()->route('sales-channels.index')
-                ->with('success', 'Successfully connected to eBay!');
-        } catch (\Exception $e) {
-            \Log::error('eBay Callback Error', ['message' => $e->getMessage()]);
+    //         return redirect()->route('sales-channels.index')
+    //             ->with('success', 'Successfully connected to eBay!');
+    //     } catch (\Exception $e) {
+    //         \Log::error('eBay Callback Error', ['message' => $e->getMessage()]);
 
-            return redirect()->route('sales-channels.index')
-                ->with('error', 'eBay authorization failed: ' . $e->getMessage());
-        }
-    }
+    //         return redirect()->route('sales-channels.index')
+    //             ->with('error', 'eBay authorization failed: ' . $e->getMessage());
+    //     }
+    // }
 
     /**
      * Return error response
      */
-    private function errorResponse(string $message, int $status = 500)
-    {
-        return response()->json([
-            'success' => false,
-            'message' => $message,
-        ], $status);
-    }
+    // private function errorResponse(string $message, int $status = 500)
+    // {
+    //     return response()->json([
+    //         'success' => false,
+    //         'message' => $message,
+    //     ], $status);
+    // }
 }
