3aa880316fafb5c0aa0fc9370070ab4fa8e8a56e
Muhammad Saifullah Asif <muhammadsaifullahasif@gmail.com>
2026-01-17 23:35:03 +0500

    Imporvements and Bug Fixes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -24,7 +24,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::latest()->paginate(25);
+        $products = Product::orderBy('id', 'DESC')->paginate(25);
         // return $products;
         return view('products.index', compact('products'));
     }
