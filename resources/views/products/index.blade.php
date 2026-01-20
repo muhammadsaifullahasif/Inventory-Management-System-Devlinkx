db977f1392132afc151485d01ed521be04aeace1
Muhammad Saifullah Asif <muhammadsaifullahasif@gmail.com>
2026-01-21 01:34:17 +0500

    Imporvements and Bug Fixes

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -67,7 +67,7 @@
                             {{-- {{ $product->sales_channels->name ?? 'N/A' }} --}}
                             {{-- {{ implode(', ', $product->sales_channels->name) }} --}}
                             @foreach ($product->sales_channels as $sales_channel)
-                                {{ $sales_channel->name }}
+                                {{ $sales_channel['name'] }}
                             @endforeach
                         </td>
                         <td>{{ \Carbon\Carbon::parse($product->created_at)->format('d M, Y') }}</td>
