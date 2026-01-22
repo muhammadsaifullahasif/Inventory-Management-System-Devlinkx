2375473054dbf996a1d297aed9365eada62c84c6
Muhammad Saifullah Asif <muhammadsaifullahasif@gmail.com>
2026-01-23 00:00:49 +0500

    Imporvements and Bug Fixes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EbayController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::post('/ebay/orders/webhook/{id}', [EbayController::class, 'handleEbayOrderWebhook'])->name('ebay.orders.webhook');
