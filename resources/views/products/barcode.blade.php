7d2e52cedaeee6f6cf0b4005b30f602ff4b05b09
Muhammad Saifullah Asif <muhammadsaifullahasif@gmail.com>
2026-01-29 20:54:23 +0500

    Imporvements and Bug Fixes

--- resources/views/products/barcode.blade.php
+++ resources/views/products/barcode.blade.php
@@ -1,22 +1,18 @@
 <div style="width: 100%; display: flex; flex-wrap: wrap; justify-content: space-evenly; align-items: center;">
-    {{-- @if ( $_GET['number_of_barcode'] != '' ) --}}
-        @for ($i = 0; $i < ( isset($_GET['number_of_barcode']) ? $_GET['number_of_barcode'] : 42 ); $i++)
-            <div style="margin: 10px; text-align: center; border: 1px solid #000; padding: 10px; max-width: 30%; width: 100%;">
-                <div style="max-width: 100%;">
-                    @php
-                        // Make Barcode object of Code128 encoding.
-                        $barcode = (new Picqer\Barcode\Types\TypeCode128())->getBarcode($product->barcode);
-                        $renderer = new Picqer\Barcode\Renderers\SvgRenderer();
-                        // $renderer->setSvgType($renderer::TYPE_SVG_INLINE); // Changes the output to be used inline inside HTML documents, instead of a standalone SVG image (default)
-                        $renderer->setSvgType($renderer::TYPE_SVG_STANDALONE); // If you want to force the default, create a stand alone SVG image
-                        // echo $renderer->render($barcode, 80, 40);
-                    @endphp
-                    {!! $renderer->render($barcode, 100, 40) !!}
-                </div>
-                <div>{{ $product->barcode }}</div>
+    @for ($i = 0; $i < ( isset($_GET['number_of_barcode']) ? $_GET['number_of_barcode'] : 42 ); $i++)
+        <div style="margin: 10px; text-align: center; border: 1px solid #000; padding: 10px; max-width: 30%; width: 100%;">
+            <div style="max-width: 100%;">
+                @php
+                    // Make Barcode object of Code128 encoding.
+                    $barcode = (new Picqer\Barcode\Types\TypeCode128())->getBarcode($product->barcode);
+                    $renderer = new Picqer\Barcode\Renderers\PngRenderer();
+                    $renderer->setForegroundColor([0, 0, 0]); // Black bars
+                    $pngData = $renderer->render($barcode, 200, 80);
+                    $base64 = base64_encode($pngData);
+                @endphp
+                <img src="data:image/png;base64,{{ $base64 }}" style="max-width: 100%; height: auto;" />
             </div>
-        @endfor
-    {{-- @else --}}
-        {{-- <p>No barcodes to display.</p> --}}
-    {{-- @endif --}}
+            <div style="margin-top: 5px; font-size: 12px;">{{ $product->barcode }}</div>
+        </div>
+    @endfor
 </div>
