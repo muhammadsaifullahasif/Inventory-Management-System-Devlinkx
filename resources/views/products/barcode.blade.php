11b94ad1a1e56cf8ba32d1e7c012d32ed3137af6
Muhammad Saifullah Asif <muhammadsaifullahasif@gmail.com>
2026-01-29 21:04:57 +0500

    Imporvements and Bug Fixes

--- resources/views/products/barcode.blade.php
+++ resources/views/products/barcode.blade.php
@@ -7,23 +7,49 @@
         margin: 0;
         padding: 0;
     }
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+    td {
+        width: 33.33%;
+        text-align: center;
+        vertical-align: top;
+        padding: 10px;
+    }
+    .barcode-box {
+        border: 1px solid #000;
+        padding: 10px;
+        margin: 5px;
+    }
+    .barcode-text {
+        margin-top: 5px;
+        font-size: 12px;
+    }
 </style>
 
-<div style="width: 100%; display: flex; flex-wrap: wrap; justify-content: space-evenly; align-items: center;">
-    @for ($i = 0; $i < ( isset($_GET['number_of_barcode']) ? $_GET['number_of_barcode'] : 42 ); $i++)
-        <div style="margin: 10px; text-align: center; border: 1px solid #000; padding: 10px; max-width: 30%; width: 100%;">
-            <div style="max-width: 100%;">
-                @php
-                    // Make Barcode object of Code128 encoding.
-                    $barcode = (new Picqer\Barcode\Types\TypeCode128())->getBarcode($product->barcode);
-                    $renderer = new Picqer\Barcode\Renderers\PngRenderer();
-                    $renderer->setForegroundColor([0, 0, 0]); // Black bars
-                    $pngData = $renderer->render($barcode, 200, 80);
-                    $base64 = base64_encode($pngData);
-                @endphp
-                <img src="data:image/png;base64,{{ $base64 }}" style="max-width: 100%; height: auto;" />
+@php
+    $numberOfBarcodes = isset($_GET['number_of_barcode']) ? $_GET['number_of_barcode'] : 42;
+    $barcode = (new Picqer\Barcode\Types\TypeCode128())->getBarcode($product->barcode);
+    $renderer = new Picqer\Barcode\Renderers\PngRenderer();
+    $renderer->setForegroundColor([0, 0, 0]);
+    $pngData = $renderer->render($barcode, 180, 60);
+    $base64 = base64_encode($pngData);
+@endphp
+
+<table>
+    @for ($i = 0; $i < $numberOfBarcodes; $i++)
+        @if ($i % 3 == 0)
+            <tr>
+        @endif
+        <td>
+            <div class="barcode-box">
+                <img src="data:image/png;base64,{{ $base64 }}" style="width: 100%; max-width: 180px;" />
+                <div class="barcode-text">{{ $product->barcode }}</div>
             </div>
-            <div style="margin-top: 5px; font-size: 12px;">{{ $product->barcode }}</div>
-        </div>
+        </td>
+        @if ($i % 3 == 2 || $i == $numberOfBarcodes - 1)
+            </tr>
+        @endif
     @endfor
-</div>
+</table>
