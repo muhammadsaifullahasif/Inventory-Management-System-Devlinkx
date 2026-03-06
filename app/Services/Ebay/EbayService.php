cb4d5d4143e731d3e6a28f86280abf8c7b2c17ab
Muhammad Saifullah Asif <muhammadsaifullahasif@gmail.com>
2026-03-06 22:58:01 +0500

    Bug Fixes

--- app/Services/Ebay/EbayService.php
+++ app/Services/Ebay/EbayService.php
@@ -409,6 +409,10 @@ class EbayService
             'orders' => [],
         ];
 
+        Log::info('Ebay Orders Reponse: ', [
+            'response' => $response,
+        ]);
+
         $orders = self::normalizeList($response['OrderArray']['Order'] ?? []);
         foreach ($orders as $order) {
             $result['orders'][] = $this->parseOrder($order);
