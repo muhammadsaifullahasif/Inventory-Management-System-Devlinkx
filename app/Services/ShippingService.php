f4848d0f0466964e59e050f812ad2d97ff296929
Muhammad Saifullah Asif <muhammadsaifullahasif@gmail.com>
2026-03-09 23:36:33 +0500

    Bug Fixes in FedEx Label Generation API

--- app/Services/ShippingService.php
+++ app/Services/ShippingService.php
@@ -612,21 +612,6 @@ class ShippingService
                         'customerReferenceType' => 'CUSTOMER_REFERENCE',
                         'value' => 'Order #' . ($order->order_number ?? $order->id),
                     ]],
-                    'specialServices' => [
-                        'specialServiceTypes' => ['EMAIL_NOTIFICATION', 'SHIPMENT_CONFIRMATION', 'EVENT_NOTIFICATION'],
-                        'eventNotifications' => [[
-                            'events' => ['ON_SHIPMENT','ON_EXCEPTION','ON_DELIVERY'],
-                            'notificationDetail' => [
-                                'notificationType' => 'EMAIL',
-                                'emailDetail' => [
-                                    'emailAddress' => 'muhammadsaifullahasif@gmail.com' ?? '',
-                                    'name' => $order->shipping_name ?? $order->buyer_name ?? 'Customer',
-                                ],
-                                'localization' => ['languageCode' => 'EN'],
-                            ],
-                            'formatSpecification' => ['type' => 'HTML'],
-                        ]],
-                    ],
                 ]],
 
                 'totalPackageCount' => 1,
