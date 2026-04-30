97369c2ace6c81b7d9726fb1238abf16115025ac
Muhammad Saifullah Asif <muhammadsaifullahasif@gmail.com>
2026-04-30 19:42:02 +0500

    Add Purchase History and Sales History in Product View Page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -486,7 +486,7 @@ class ProductController extends Controller
             'order_count' => $orderHistory->unique('order_id')->count(),
         ];
 
-        return $orderHistory;
+        return $product;
 
         return view('products.show', compact('product', 'purchaseHistory', 'purchaseStats', 'orderHistory', 'orderStats'));
     }
