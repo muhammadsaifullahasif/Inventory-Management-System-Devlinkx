e80b4e74a1c96d2a109e05cab745ad0207838063
Muhammad Saifullah Asif <muhammadsaifullahasif@gmail.com>
2026-07-29 20:38:41 +0500

    Bug Fixes in Cron Jobs

--- queue-worker.sh
+++ queue-worker.sh
@@ -2,4 +2,4 @@
 
 cd /home/u776021627/domains/inventory.satmec.com/public_html || exit 1
 
-/usr/local/bin/php /home/u776021627/domains/://satmec.com queue:work --queue=ebay-imports,inventory-sync,default --stop-when-empty --max-time=50 --timeout=1800
+/usr/local/bin/php /home/u776021627/domains/://satmec.com queue:work database --queue=ebay-imports --queue=inventory-sync --queue=default --stop-when-empty --max-time=50 --timeout=1800
